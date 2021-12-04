6cacbb55ac5854bae3fd2a0db5fd46507d8de85c
Naveent97 <tnaveen1997@gmail.com>
2021-12-04 20:24:57 +0530

    Update

--- TrailProject/JavaScript/script.js
+++ TrailProject/JavaScript/script.js
@@ -7,3 +7,14 @@ function changeBack(){
   var label1 = document.getElementById('mainHeading');
   label1.innerHTML = "Hello";
 }
+
+function addLabel() {
+  var newDef1 = document.createElement('td');
+  var newLabel1 = document.createElement('label');
+  newLabel1.innerHTML = "Hello There";
+  //var newText1 = document.createTextNode("Water");
+  var exRow1 = document.getElementById('secondRow');
+
+  newDef1.appendChild(newLabel1);
+  exRow1.appendChild(newDef1);
+}
